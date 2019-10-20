a8cce68b1b6925eb8745f0cee31bfbfe956b07d7
Andrew Hughes <notMe@Jessicas-MacBook-Pro.local>
2019-10-19 22:35:52 -0700

    Echo first pass

--- sketch_oct19a/sketch_oct19a.ino
+++ sketch_oct19a/sketch_oct19a.ino
@@ -6,13 +6,34 @@ const int ECHO = 5;
 
 Servo servo;
 
+long duration;
+int distance;
+
+void checkEcho() {
+  digitalWrite(TRIGGER, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIGGER, LOW);
+
+  duration = pulseIn(ECHO, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+
+}
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
   
   servo.attach(SERVO);
 }
 
 void loop() {
-//  servo.write(random(0, 360));
-  delay(500);
+  servo.write(random(180));
+  delay(random(100, 1000));
 }
