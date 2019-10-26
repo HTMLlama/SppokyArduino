0bcff132d10f69602aa833ae622deb23e291e4bd
HTMLlama <andrew@hugheswd.com>
2019-10-26 10:07:24 -0700

    Working led eyes

--- sketch_oct19a/sketch_oct19a.ino
+++ sketch_oct19a/sketch_oct19a.ino
@@ -4,10 +4,16 @@ const int SERVO = 9;
 const int TRIGGER = 3;
 const int ECHO = 5;
 
+const int RED = 9;
+
+int counter = 0;
+
 Servo servo;
 
 long duration;
 int distance;
+
+int redBgt = 0;
 
 void runServo() {
   servo.write(1);
@@ -20,29 +26,39 @@ void runServo() {
   delay(500);
 }
 
+void setLed() {
+
+  if(distance < 100) {
+    redBgt = map(distance, 100, 0, 50, 250);
+    Serial.print(" Brightness: ");
+    Serial.print(redBgt);
+  } else {
+    redBgt = 1;  
+  }
+
+  analogWrite(RED, redBgt);
+}
+
 void checkEcho() {
+
   digitalWrite(TRIGGER, LOW);
   delayMicroseconds(2);
-
   digitalWrite(TRIGGER, HIGH);
   delayMicroseconds(10);
-
   digitalWrite(TRIGGER, LOW);
 
   duration = pulseIn(ECHO, HIGH);
   distance = duration * 0.034 / 2;
+  
   Serial.print("\n Distance: ");
   Serial.print(distance);
-
-  if(distance < 20) {
-    runServo();  
-  }
-
 }
 
 void setup() {
   Serial.begin(9600);
 
+  pinMode(RED, OUTPUT);
+  
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
   
@@ -50,7 +66,12 @@ void setup() {
 }
 
 void loop() {
-//  servo.write(random(180));
-  checkEcho();
-  delay(150);
+//  if(counter >= 200) {
+    checkEcho();
+    counter = 0;
+//  }
+//  counter++;
+  
+  setLed();
+//  delay(30);
 }
